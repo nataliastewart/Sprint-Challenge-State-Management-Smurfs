f49aec4163a33c6338a1d1f67ada359fef3ab4ef
nataliastewart <nataliastewart@users.noreply.github.com>
2020-04-17 12:31:40 -0400

    Reducer logic DONE

--- smurfs/src/reducers/smurfReducer.js
+++ smurfs/src/reducers/smurfReducer.js
@@ -1,11 +1,42 @@
+import {
+  FETCH_SMURFS_START,
+  FETCH_SMURFS_SUCCESS,
+  FETCH_SMURFS_FAIL,
+} from "../actions";
+
 //setup my empty reducer and initial state
 
 const initialState = {
-  smurfs: {},
+  smurfs: [],
   fetchingSmurfs: false,
   error: "",
 };
 
-const reducer = (state = initialState, action) => {};
+//write logic for actions
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCH_SMURFS_START:
+      return {
+        ...state,
+        fetchingSmurfs: true,
+      };
+    case FETCH_SMURFS_SUCCESS:
+      return {
+        ...state,
+        smurfs: action.payload,
+        fetchingSmurfs: false,
+        error: "",
+      };
+    case FETCH_SMURFS_FAIL:
+      return {
+        ...state,
+        fetchingSmurfs: false,
+        error: action.payload,
+      };
+    default:
+      return state;
+  }
+};
 
 export default reducer;
