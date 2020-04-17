505da5866fc6c42470d4ece97cbcaa69e44d63a3
nataliastewart <nataliastewart@users.noreply.github.com>
2020-04-17 11:52:01 -0400

    Displayed the first data on the browser - Open src/index.js to make sure that your app is ready to roll with the proper middleware. DONE

---
+++ smurfs/src/reducers/smurfReducer.js
@@ -0,0 +1,11 @@
+//setup my empty reducer and initial state
+
+const initialState = {
+  smurfs: {},
+  fetchingSmurfs: false,
+  error: "",
+};
+
+const reducer = (state = initialState, action) => {};
+
+export default reducer;
